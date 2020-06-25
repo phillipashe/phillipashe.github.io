ed74efb08b1224bc1f8f11c0d653c2a20e67dd3c
Phillip Ashe <ashephillip@gmail.com>
2020-06-25 19:35:34 -0400

    attempt with svg corner

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+// $(document).ready(function() {
+//     // $('#top-wave').height($(window).height());
+//     document.getElementById('#top-wave').setAttribute("height", $(window).height())
+// });
+
+shape = document.getElementById("top-wave");
+shape.setAttribute("viewBox", "0 0 500 200");
