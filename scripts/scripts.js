2b3e0745d287479e4a80786884e12c4993f3834d
Phillip Ashe <ashephillip@gmail.com>
2020-07-14 21:58:52 -0400

    implemented animation for text

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,5 +3,13 @@
 //     document.getElementById('#top-wave').setAttribute("height", $(window).height())
 // });
 
-shape = document.getElementById("top-wave");
-shape.setAttribute("viewBox", "0 0 500 200");
+// shape = document.getElementById("top-wave");
+// shape.setAttribute("viewBox", "0 0 500 200");
+
+var waypoint = new Waypoint({
+    element: document.getElementById("test"),
+    handler: function() {
+        alert('Basic waypoint triggered');
+    },
+    // offset: 1000
+})
