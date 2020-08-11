b66b91edb7f96f0b500c97bddeaa352fdd72aef2
Phillip Ashe <ashephillip@gmail.com>
2020-08-11 17:03:26 -0400

    unused script removal, comment removal & typo

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,3 @@
-// $(document).ready(function() {
-//     // $('#top-wave').height($(window).height());
-//     document.getElementById('#top-wave').setAttribute("height", $(window).height())
-// });
-
-// shape = document.getElementById("top-wave");
-// shape.setAttribute("viewBox", "0 0 500 200");
-
-// var waypoint = new Waypoint({
-//     element: document.getElementById("test"),
-//     handler: function() {
-//         alert('Basic waypoint triggered');
-//     },
-//     // offset: 1000
-// })
-
 $(document).ready(function () {
     $("#ajax-body").load("../static/about.html");
     $("#about").click(function () {
